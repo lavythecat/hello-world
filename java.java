6125933432d904f904ed9ef718cee854f5d4e28c
meltedchocolate <meltedchocolate@users.noreply.github.com>
2015-09-01 14:18:07 -0400

    Create java.java

---
+++ java.java
@@ -0,0 +1,10 @@
+/*
+Java Hello world
+by Ricky LeDew (meltedchocolate)
+*/
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
