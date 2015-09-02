1ab5c0a5da1a6077a7624097e7312d61fcff061f
meltedchocolate <aledew@comcast.net>
2015-09-02 13:58:14 -0400

    Fixed c++ and tested everything

--- c.c
+++ c.c
@@ -5,6 +5,6 @@ by Ricky LeDew (meltedchocolate)
 #include <stdio.h> //include the standard I/O so we can print things out
 
 int main() { //Main execution, is integer because it has to return an integer status code
-  printf("Hello, world!"); //Print out the message
+  printf("Hello, world!\n"); //Print out the message with newline character (\n)
   return 0; //Say to the system that the program exited successfully (status code 0)
 }
